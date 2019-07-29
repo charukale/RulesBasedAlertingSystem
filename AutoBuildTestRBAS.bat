534ce83f395a5a65a4afd59945cf12b5f78ad6e3
charukale <charu.kale003@gmail.com>
2019-07-30 01:17:08 +0530

    Delete AutoBuildTestRBAS.bat

--- AutoBuildTestRBAS.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Building RulesBasedAlertingSystem solution.
-"C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" RulesBasedAlertingSys.sln /build debug
-echo Build succeeded.
-echo Running application
-"bin\RulesBasedAlertingSystem.exe"
